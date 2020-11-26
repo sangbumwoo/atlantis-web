0742e036cdc809fc6d5cdfc001ce18887f5ba4ab
Sangbum Woo <Sangbum.Woo@partner.bmw.co.kr>
2020-11-27 00:13:35 +0900

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'kakao-serices-web';
+  title = 'angular-routing-example';
 }
