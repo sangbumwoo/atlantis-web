a16a44fb3c841d739ae555b59ee699de42fd1670
Sangbum Woo <Sangbum.Woo@partner.bmw.co.kr>
2020-12-20 18:04:43 +0900

    Bootstrap 4 적용

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import {RouterModule, Routes} from '@angular/router';
-import {LoginModule} from './login/login.module';
-import {DashboardModule} from './dashboard/dashboard.module';
-import {RegistrationModule} from './registration/registration.module';
-import {LayoutModule} from './layout/layout.module';
-import {UsersModule} from './users/users.module';
-import {AccountSettingsModule} from './account-settings/account-settings.module';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginModule } from './login/login.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { RegistrationModule } from './registration/registration.module';
+import { LayoutModule } from './layout/layout.module';
+import { UsersModule } from './users/users.module';
+import { AccountSettingsModule } from './account-settings/account-settings.module';
 
 const routes: Routes = [
   {
@@ -24,6 +24,7 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     RouterModule.forRoot(routes),
     LayoutModule,
     LoginModule,
